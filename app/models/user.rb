391e92184d1c689e9613ed7d9980325f0b9733cf
Kikuno Ono <k-ono-9g6@eagle.sophia.ac.jp>
2024-06-04 21:03:46 +0900

    Userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-        has_many :dogs, dependent: :destroy
-        has_many :likes, dependent: :destroy
-        has_many :liked_dogs, through: :likes, source: :dog
-        has_many :comments, dependent: :destroy
+  has_many :dogs, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_dogs, through: :likes, source: :dog
+  has_many :comments, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :profile, length: { maximum: 200 }
+
   def already_liked?(dog)
     self.likes.exists?(dog_id: dog.id)
   end
-  has_many :dogs, dependent: :destroy
-  validates :name, presence: true
-  validates :profile, length: { maximum: 200 }
 
 end
